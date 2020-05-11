4f758a81d36228ae130ddccbc3719c17b784c16d
vorixo <aja10@alu.ua.es>
2020-05-11 16:59:52 +0200

    [FIX] WIP 1 fftw impl

--- fast_fourier_transform_bench/openmp/lib_omp_lib.cpp
+++ fast_fourier_transform_bench/openmp/lib_omp_lib.cpp
@@ -36,20 +36,24 @@ void execute_kernel(GraficObject *device_object, int64_t size)
 
     // FFTW implementation - First draft.
 	fftw_plan plan;
-	fftw_complex *in = (fftw_complex*) fftw_malloc(sizeof(fftw_complex)*size);
-	fftw_complex *out_data = (fftw_complex*) fftw_malloc(sizeof(fftw_complex)*size);
+	fftw_complex signal[size];
+    fftw_complex result[size];
+
+	plan = fftw_plan_dft_1d(size,signal,result,FFTW_FORWARD,FFTW_ESTIMATE);
+	
 	for (int i=0; i<size; ++i)
 	{
-		in[i][0] = device_object->d_B[i];
+		signal[i] = device_object->d_B[i];
 	}
-	plan = fftw_plan_dft_1d(size,in,out_data,FFTW_FORWARD,FFTW_ESTIMATE);
+
 	fftw_execute(plan);
+	
 	for (int i=0; i<size; ++i)
 	{
-		device_object->d_Br[i] = out_data[i][0];
+		device_object->d_Br[i] = result[i];
 	}
+	
 	fftw_destroy_plan(plan);
-    fftw_free(in); fftw_free(out_data);
 
 	// End compute timer
 	device_object->elapsed_time = omp_get_wtime() - start_wtime;
